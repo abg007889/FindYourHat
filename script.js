7f792512f7c235791e98b0657cf44684e0d93930
abg007889 <abg007889@yahoo.com.hk>
2022-08-23 20:44:40 +0800

    add input box
    add input box let it can be play on mobile

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let arrImg = ["img/wall.png", "img/road.png", "img/walk.png", "img/hat.png", "im
 let end = false, map = [], x, y;
 document.addEventListener("keydown", direction);
 var txt = document.getElementById('text');
+var boxBtn = document.getElementById('boxBtn');
+
+boxBtn.addEventListener('click', () => {
+  document.getElementById('inputBox').hidden = false;
+  boxBtn.hidden = true;
+}, false)
 
 // After win or lose
 function newGame() {
@@ -67,13 +73,13 @@ function GenerateMap() {
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map.length; j++) {
       if (map[i][j] === 'wall')
-        $(".map").append(`<img id='${i}${j}' src=${arrImg[0]} alt='wall' width='50' height='50'>`);
+        $(".map").append(`<img id='${i}${j}' src=${arrImg[0]} class='img' alt='wall'>`);
       if (map[i][j] === 'road')
-        $(".map").append(`<img id='${i}${j}' src=${arrImg[1]} alt='road' width='50' height='50'>`);
+        $(".map").append(`<img id='${i}${j}' src=${arrImg[1]} class='img' alt='road'>`);
       if (map[i][j] === 'player')
-        $(".map").append(`<img id='${i}${j}' src=${arrImg[2]} alt='player' width='50' height='50'>`);
+        $(".map").append(`<img id='${i}${j}' src=${arrImg[2]} class='img' alt='player'>`);
       if (map[i][j] === 'hat' || map[i][j] === 'hole')
-        $(".map").append(`<img id='${i}${j}' src=${arrImg[6]} alt='quest' width='50' height='50'>`);
+        $(".map").append(`<img id='${i}${j}' src=${arrImg[6]} class='img' alt='quest'>`);
     }
     $(".map").append("<br/>");
   }
@@ -84,6 +90,7 @@ GenerateMap();
 // Direction and check win or lose
 function direction(e) {
   if (end) e.keydown(newGame());  // it will start newGame function if win or lose
+  if (inputBox.value) inputBox.value = '';
   if (e.code === 'KeyA') {
     if (y != 0) {
       y -= 1;
